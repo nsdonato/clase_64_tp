004b73bea88e718cbaa80199f448c929dddfdc94
Noelia Donato <noeliadonato@gmail.com>
2020-01-20 20:47:36 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,79 @@
+const gatos = [
+    {
+        name: "Rodolfo",
+        shortDesc: "Tiene 4 años, le gusta perseguir mariposas, se lleva bien con niños y con otros gatos.",
+        longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
+        img: "http://www.placekitten.com/400"
+    },
+
+    {
+        name: "Muzzarella",
+        shortDesc: "Muy dulce y mimosa. Tiene seis dedos en una pata que asegura le dan superpoderes.",
+        longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
+        img: "http://www.placekitten.com/500"
+    },
+
+    {
+        name: "Artilugia",
+        shortDesc: "Muy activa y juguetona. Se lleva bien con perros. Ideal para casa con jardin amplio",
+        longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
+        img: "http://www.placekitten.com/600"
+    },
+
+    {
+        name: "Wosito",
+        shortDesc: "Vivio toda su vida en la calle y todavia se asombra de cosas como estufas y escaleras.",
+        longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
+        img: "http://www.placekitten.com/700"
+    },
+
+    {
+        name: "Calamardo",
+        shortDesc: "Dicen que de noche, cuando nadie lo puede escuchar, invoca a Cthulu. Muy mimoso.",
+        longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
+        img: "http://www.placekitten.com/700"
+    },
+]
+
+let acumuladora = "";
+const btnAbrirModal = document.getElementsByTagName("button");
+const modal = document.getElementById("modal");
+const modalContenedor = document.getElementById("modalContenedor");
+const contenedorCards = document.getElementById("contenedorCards");
+
+for (let i = 0; i < gatos.length; i++) {
+    acumuladora += `
+  <div id="card">
+    <div class="img">
+        <img src="${gatos[i].img}" alt="" />
+    </div>
+    <div class="info">
+        <h1>${gatos[i].name}</h1>
+        <p>
+        ${gatos[i].shortDesc}
+        </p>
+        <button>Ver mas</button>
+    </div>
+  </div>`;
+
+}
+
+contenedorCards.innerHTML = acumuladora;
+
+for (let x = 0; x < gatos.length; x++) {
+    btnAbrirModal[x].onclick = (e) => {
+
+        modalContenedor.innerHTML = `<div>
+        <h1>Adopta un gatito</h1>
+        <h6>${gatos[x].name}</h6>
+        <p>
+        ${gatos[x].longDesc}
+        </p>
+        </div>
+        <div class="img">
+        <img src="${gatos[x].img}" alt="" />
+        </div>`;
+
+        modal.classList.remove("nomostrar");
+    }
+}
