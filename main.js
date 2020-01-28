37298b70cea0018e72ed1acbce72197f27fd35f8
Noelia Donato <noeliadonato@gmail.com>
2020-01-27 21:28:34 -0300

    gatites

--- main.js
+++ main.js
@@ -3,35 +3,45 @@ const gatos = [
         name: "Rodolfo",
         shortDesc: "Tiene 4 años, le gusta perseguir mariposas, se lleva bien con niños y con otros gatos.",
         longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
-        img: "http://www.placekitten.com/400"
+        img: "./img/gatitos/gatito_blanco.jpg",
+        colors: ["blanco"],
+        sex: "female"
     },
 
     {
         name: "Muzzarella",
         shortDesc: "Muy dulce y mimosa. Tiene seis dedos en una pata que asegura le dan superpoderes.",
         longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
-        img: "http://www.placekitten.com/500"
+        img: "./img/gatitos/gatito_naranja.jpg",
+        colors: ["naranja", "blanco"],
+        sex: "male"
     },
 
     {
         name: "Artilugia",
         shortDesc: "Muy activa y juguetona. Se lleva bien con perros. Ideal para casa con jardin amplio",
         longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
-        img: "http://www.placekitten.com/600"
+        img: "./img/gatitos/gatito_gris_blanco.jpg",
+        colors: ["blanco", "gris"],
+        sex: "female"
     },
 
     {
         name: "Wosito",
         shortDesc: "Vivio toda su vida en la calle y todavia se asombra de cosas como estufas y escaleras.",
         longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
-        img: "http://www.placekitten.com/700"
+        img: "./img/gatitos/gatito_rayado.jpg",
+        colors: ["negro", "blanco", "gris"],
+        sex: "female"
     },
 
     {
         name: "Calamardo",
         shortDesc: "Dicen que de noche, cuando nadie lo puede escuchar, invoca a Cthulu. Muy mimoso.",
         longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
-        img: "http://www.placekitten.com/700"
+        img: "./img/gatitos/gatito_negro.jpg",
+        colors: ["negro"],
+        sex: "male"
     },
 ]
 
@@ -55,12 +65,12 @@ for (let i = 0; i < gatos.length; i++) {
         <button class="ver">Ver mas</button>
     </div>
   </div>`;
-
 }
 
 contenedorCards.innerHTML = acumuladora;
 
 for (let x = 0; x < gatos.length; x++) {
+
     btnAbrirModal[x].onclick = (e) => {
 
         modalContenedor.innerHTML = `
@@ -76,7 +86,49 @@ for (let x = 0; x < gatos.length; x++) {
         modal.classList.remove("nomostrar");
     }
 }
-const form = document.querySelector(`form`);
+
+const form = document.forms[0];
+
 form.onsubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+
+    const checkboxes = document.querySelectorAll("input[type='checkbox']");
+    const radios = document.querySelectorAll("input[type='radio']");
+    const inputs = document.querySelectorAll("input[type='text']");
+
+    let opcionesElegidas = [];
+
+    for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            opcionesElegidas.push(checkboxes[i].value);
+        }
+    }
+
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            opcionesElegidas.push(radios[i].value);
+        }
+    }
+
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value != "") {
+            opcionesElegidas.push(inputs[i].value);
+        }
+    }
+
+    console.log(opcionesElegidas);
 }
+
+// Como vamos a filtrar nuestros gatos por color y por sexo, 
+// necesitamos agregar esos campos a los objetos
+// 1. A cada objeto gatuno agregarle un array de colores (los eligen ustedes) y el sexo
+// Por ejemplo un gato negro y blanco 
+// colores: ["negro", "blanco"]
+// sexo: "m"
+// y una gata tricolor
+// colores: ["negro", "blanco", "rayado", "naranja"]
+// sexo: "f"
+// Consejo: reemplacen las imagenes de placekitten por las de su preferencia
+// que se adecuen a la descripcion que vamos a dar 
+// Mostrar en consola la eleccion del usuario al hacer clic en "filtrar"
+// Nombre, telefono, colores de los gatitos y sexo elegido
